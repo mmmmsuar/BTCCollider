a389585ae2a61f084bf1bc5a5a86ce0ace8df4c0
mmmmsuar <168681351+mmmmsuar@users.noreply.github.com>
2024-09-05 23:11:41 -0400

    Update SECP256k1.h

--- SECP256k1.h
+++ SECP256k1.h
@@ -15,8 +15,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef SECP256k1H
-#define SECP256k1H
+#ifndef SECP256K1H
+#define SECP256K1H
 
 #include "Point.h"
 #include <string>
@@ -37,9 +37,10 @@ public:
   Point ComputePublicKey(Int *privKey);
   Point NextKey(Point &key);
   void Check();
-  bool  EC(Point &p);
+  bool EC(Point &p);
 
-  void GetHash160(int type,bool compressed,
+  // For address generation
+  void GetHash160(int type, bool compressed,
     Point &k0, Point &k1, Point &k2, Point &k3,
     uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
 
@@ -48,37 +49,42 @@ public:
     bool y0Odd, bool y1Odd, bool y2Odd, bool y3Odd,
     uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3);
 
-  void GetHash160(int type,bool compressed, Point &pubKey, unsigned char *hash);
+  void GetHash160(int type, bool compressed, Point &pubKey, unsigned char *hash);
+  void GetCompressedHash160(int type, Int *x, bool yOdd, unsigned char *hash);
 
-  void GetCompressedHash160(int type,Int *x, bool yOdd, unsigned char *hash);
-
+  // For generating Bitcoin addresses
   std::string GetAddress(int type, bool compressed, Point &pubKey);
   std::string GetAddress(int type, bool compressed, unsigned char *hash160);
   std::vector<std::string> GetAddress(int type, bool compressed, unsigned char *h1, unsigned char *h2, unsigned char *h3, unsigned char *h4);
-  std::string GetPrivAddress(bool compressed, Int &privKey );
+  std::string GetPrivAddress(bool compressed, Int &privKey);
   std::string GetPublicKeyHex(bool compressed, Point &p);
   Point ParsePublicKeyHex(std::string str, bool &isCompressed);
 
   bool CheckPudAddress(std::string address);
 
-  static Int DecodePrivateKey(char *key,bool *compressed);
+  static Int DecodePrivateKey(char *key, bool *compressed);
 
+  // Elliptic Curve Operations
   Point Add(Point &p1, Point &p2);
   Point Add2(Point &p1, Point &p2);
   Point AddDirect(Point &p1, Point &p2);
   Point Double(Point &p);
   Point DoubleDirect(Point &p);
 
-  Point G;                 // Generator
-  Int   order;             // Curve order
+  // Use toString() for any serialization or output needs
+  std::string toString() {
+    return "SECP256K1 object";  // Modify this based on your needs
+  }
+
+  Point G;   // Generator
+  Int order; // Curve order
 
 private:
 
-  uint8_t GetByte(std::string &str,int idx);
-
+  uint8_t GetByte(std::string &str, int idx);
   Int GetY(Int x, bool isEven);
-  Point GTable[256*32];       // Generator table
-
+  Point GTable[256 * 32]; // Generator table
 };
 
-#endif // SECP256k1H
+#endif // SECP256K1H
+
